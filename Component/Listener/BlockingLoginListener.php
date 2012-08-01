e6506cf4eb326c5daf733f53371c5953fbd5e835
Reece Fowell <reece@codeconsortium.com>
2012-08-01 03:55:26 +0100

    Add redirect on too many login failures to BlockingLoginListener

---
+++ Component/Listener/BlockingLoginListener.php
@@ -0,0 +1,122 @@
+<?php
+
+/*
+ * This file is part of the CCDNUser SecurityBundle
+ *
+ * (c) CCDN (c) CodeConsortium <http://www.codeconsortium.com/> 
+ * 
+ * Available on github <http://www.github.com/codeconsortium/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CCDNUser\SecurityBundle\Component\Listener;
+
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * 
+ * @author Reece Fowell <reece@codeconsortium.com> 
+ * @version 1.0
+ */
+class BlockingLoginListener
+{
+
+	
+	
+	/**
+	 *
+	 * @access protected
+	 */
+	protected $container;
+
+
+
+	/**
+	 *
+	 * @access protected
+	 */
+	protected $router;
+
+
+
+	/**
+	 *
+	 * @access public
+	 * @param $router, $container
+	 */
+	public function __construct($container, $router)
+	{
+
+		$this->container = $container;		
+
+		$this->router = $router;
+	}
+
+
+
+	/**
+	 *
+	 * @access public
+	 * @param GetResponseEvent $event
+	 */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+	
+		// Abort if we are dealing with some symfony2 internal requests.
+		if ($event->getRequestType() !== \Symfony\Component\HttpKernel\HttpKernel::MASTER_REQUEST) {
+			return;
+		}
+
+		// Get the route from the request object.
+        $request = $this->container->get('request');
+
+		$route = $request->get('_route');
+
+		$blockRoutes = $this->container->getParameter('ccdn_user_security.login_shield.block_routes_when_denied');
+		
+		// Abort if the route is not a login route.
+		if ( ! in_array($route, $blockRoutes)) {
+			return;
+		}
+
+		// Set a limit on how far back we want to look at failed login attempts.
+		$blockInMinutes = $this->container->getParameter('ccdn_user_security.login_shield.block_for_minutes');
+
+		$timeLimit = new \DateTime('-' . $blockInMinutes . ' minutes');
+
+		// Get session and check if it has any entries of failed logins.
+		$session = $request->getSession();
+
+		// Only load from the db if the session is not found.
+		if ($session->has('auth_failed')) {
+			$attempts = $session->get('auth_failed');
+	
+			// Iterate over attempts and only reveal attempts that fall within the $timeLimit.
+	
+		} else {
+			$ipAddress = $request->getClientIp();
+	
+			$attempts = $this->container->get('ccdn_user_security.session.repository')->findByIpAddress($ipAddress, $timeLimit);				
+		}
+
+
+		$attemptLimitRecoverAccount = $this->container->getParameter('ccdn_user_security.login_shield.limit_failed_login_attempts.before_recover_account');
+
+		if (count($attempts) > $attemptLimitRecoverAccount)
+		{
+			$recoverAccountRouteName = $this->container->getParameter('ccdn_user_security.login_shield.recover_account_route.name');
+			$recoverAccountRouteParams = $this->container->getParameter('ccdn_user_security.login_shield.recover_account_route.params');
+
+			$event->setResponse(new RedirectResponse($this->container->get('router')->generate($recoverAccountRouteName, $recoverAccountRouteParams)));
+
+		}
+
+		return;
+	
+    }
+
+}
---
+++ Component/Listener/RouteRefererListener.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+ * This file is part of the CCDNUser SecurityBundle
+ *
+ * (c) CCDN (c) CodeConsortium <http://www.codeconsortium.com/> 
+ * 
+ * Available on github <http://www.github.com/codeconsortium/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CCDNUser\SecurityBundle\Component\Listener;
+
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+/**
+ * 
+ * @author Reece Fowell <reece@codeconsortium.com> 
+ * @version 1.0
+ */
+class RouteRefererListener
+{
+
+	
+	
+	/**
+	 *
+	 * @access protected
+	 */
+	protected $container;
+
+
+
+	/**
+	 *
+	 * @access protected
+	 */
+	protected $router;
+
+
+
+	/**
+	 *
+	 * @access public
+	 * @param $router, $container
+	 */
+	public function __construct($container, $router)
+	{
+
+		$this->container = $container;		
+
+		$this->router = $router;
+	}
+
+
+
+	/**
+	 *
+	 * @access public
+	 * @param GetResponseEvent $event
+	 */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+	
+		// Abort if we are dealing with some symfony2 internal requests.
+		if ($event->getRequestType() !== \Symfony\Component\HttpKernel\HttpKernel::MASTER_REQUEST) {
+			return;
+		}
+
+		// Get the route from the request object.
+		$request = $event->getRequest();
+		
+		$route = $request->get('_route');
+
+		// Get the list of routes we must ignore.
+		$logIgnore = $this->container->getParameter('ccdn_user_security.do_not_log_route');
+			
+		// Abort if the route is ignorable.
+		foreach($logIgnore as $ignore) {
+			if ($route == $ignore['route']) { return; }
+		}
+		
+		// Check for any internal routes.
+		if ($route[0] == '_') { return; }
+		
+		// Get the session and assign it the url we are at presently.
+		$session = $request->getSession();
+		
+		$session->set('referer', $request->getBasePath() . $request->getPathInfo());			
+
+		return;
+    }
+
+}
