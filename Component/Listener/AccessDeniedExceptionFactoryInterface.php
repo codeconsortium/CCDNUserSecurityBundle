65bb7576cbeba223d5e9f6409ae2d5ac6f44f4ad
Kristian Zondervan <kristian.zondervan@gmail.com>
2014-04-17 08:35:03 +0000

    Introduce exception factory to allow customization off exception thrown when ip is blocked

---
+++ Component/Listener/AccessDeniedExceptionFactoryInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the CCDNUser SecurityBundle
+ *
+ * (c) CCDN (c) CodeConsortium <http://www.codeconsortium.com/>
+ *
+ * Available on github <http://www.github.com/codeconsortium/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CCDNUser\SecurityBundle\Component\Listener;
+
+interface AccessDeniedExceptionFactoryInterface
+{
+    /**
+     * Create exception thrown when a ip is blocked
+     *
+     * @return \Exception
+     */
+    public function createAccessDeniedException();
+}
